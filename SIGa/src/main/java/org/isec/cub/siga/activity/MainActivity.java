a8b7942bc57efffbc009fdf3ee6c3eee4c8e259e
Ricardo Silva <silvaric@gmail.com>
2014-06-09 19:18:06 +0100

    add new menu and activity

--- SIGa/src/main/java/org/isec/cub/siga/activity/MainActivity.java
+++ SIGa/src/main/java/org/isec/cub/siga/activity/MainActivity.java
@@ -8,8 +8,12 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import com.parse.Parse;
+
+import org.isec.cub.siga.SobreActivity;
 import org.isec.cub.siga.service.MyService;
+
 import java.util.Calendar;
 
 public class MainActivity extends ActionBarActivity {
@@ -61,7 +65,9 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == org.isec.cub.siga.R.id.action_settings) {
+        if (id == org.isec.cub.siga.R.id.action_settings2) {
+            Intent intent = new Intent(this, SobreActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
